49a1cdeeb143f1caee7191bcb27ac76af5ebd472
suj00033 <suj00033@naver.com>
2022-11-07 17:20:05 +0900

    1107
    board(reply table)

--- src/main/webapp/WEB-INF/sql/bank_Board.sql
+++ src/main/webapp/WEB-INF/sql/bank_Board.sql
@@ -78,4 +78,19 @@ WHERE title LIKE '%keyword%'
    OR writer LIKE '%keyword%';
    
 DESC Board;
-select * from Board
+
+
+USE prj1;
+
+-- 댓글 테이블 만들기
+CREATE TABLE Reply (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    boardId INT NOT NULL,
+    content VARCHAR(1000) NOT NULL,
+    inserted DATETIME DEFAULT NOW(),
+    FOREIGN KEY (boardId) REFERENCES Board(id)
+);
+
+DESC Reply;
+
+SELECT * FROM Reply;
