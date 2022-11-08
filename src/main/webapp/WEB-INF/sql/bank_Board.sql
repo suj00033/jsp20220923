114cfedf6362f6a53b8894ba39b272ac3ac18b66
suj00033 <suj00033@naver.com>
2022-11-08 16:41:53 +0900

    댓글수, 파일테이블

--- src/main/webapp/WEB-INF/sql/bank_Board.sql
+++ src/main/webapp/WEB-INF/sql/bank_Board.sql
@@ -94,3 +94,43 @@ CREATE TABLE Reply (
 DESC Reply;
 
 SELECT * FROM Reply;
+
+-- 댓글 수가 결과로 같이 나오는 Board Table 조회 쿼리 작성
+SELECT
+    b.id,
+    b.title,
+    b.writer,
+    b.inserted,
+    COUNT(r.id) countReply
+FROM Board b LEFT JOIN Reply r On b.id = r.boardId
+GROUP BY b.id
+ORDER BY b.id DESC;
+-- 댓글없는 게시물도 가져와야하므로 LEFT JOIN을 사용
+-- 게시글 id로 댓글 카운트
+
+-- 댓글 입력 시간 변경
+SELECT * FROM Reply WHERE boardId = 1019 ORDER BY id DESC;
+UPDATE Reply SET inserted = DATE(NOW()-INTERVAL 6 DAY) WHERE id = 60;
+UPDATE Reply SET inserted = DATE(NOW()-INTERVAL 15 DAY) WHERE id = 59;
+UPDATE Reply SET inserted = DATE(NOW()-INTERVAL 60 DAY) WHERE id = 58;
+UPDATE Reply SET inserted = DATE(NOW()-INTERVAL 400 DAY) WHERE id = 57;
+UPDATE Reply SET inserted = DATE(NOW()-INTERVAL 800 DAY) WHERE id < 57;
+
+-- 파일 테이블 만들기
+CREATE TABLE File (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    boardId INT NOT NULL,
+    name VARCHAR(512) NOT NULL,
+    FOREIGN KEY (boardId) REFERENCES Board(id)
+);
+
+DESC File;
+SELECT * FROM Reply;
+
+
+
+DESC Board;
+
+SELECT * FROM File ORDER BY 1 DESC;
+
+-- 여러 파일첨부를 보는 쿼리
